13b77f6d5feefec7e9dc080d11636c4d57f5d1ad
luckylinwei <luckylinwei@sina.cn>
2015-12-12 19:51:59 +0800

    First Commit

---
+++ 1.py
@@ -0,0 +1,16 @@
+__author__ = 'Lin'
+
+import ngram
+
+def string_similarity_ngram(str1, str2, param_N):
+    n = ngram.NGram([str1], N = param_N)
+    number_of_shared_ngrams = n.items_sharing_ngrams(str2)
+    len_of_str1 = len(list(n._split(str1)))
+    len_of_str2 = len(list(n._split(str2)))
+    nagram_similarity = 2 * number_of_shared_ngrams / (len_of_str1 + len_of_str2)
+    return nagram_similarity
+
+str1 = "verticalDrawAction"
+str2 = "drawVerticalAction"
+print(string_similarity_ngram(str1, str2, 2))
+#This is the first commit
